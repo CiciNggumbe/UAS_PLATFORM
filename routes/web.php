0053424de56f88c1d26c9b40a6e7f5d4555c40c4
mariarevani50 <mariarevani50@gmail.com>
2025-06-03 17:38:46 +0700

    menyelesaikan halaman home

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\View;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', function (): View {
+    return view('home');
 });
