25c444030036f653059b5ebed2c8432d1dce52be
delvianalipat <delvianalipat@gmail.com>
2025-06-03 17:38:37 +0700

    menyelesaikan halaman login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\View;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', function () {
+    return view('login');
 });
